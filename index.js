3cffbaa12f6e484421df8ea5f23a7338a5ef4cf7
Travis Hanneman <thanneman1@gmail.com>
2019-10-03 10:55:39 -0700

    updated structure on the add item li

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ function shoppingList () {
 
         const newItem = $(this).find('#shopping-list-entry').val();
 
-        $('.shopping-list').append(`<li><span class="shopping-item">${newItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        $('.shopping-list').append(`<li>
+            <span class="shopping-item">${newItem}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`);
     });
 
     // Check an item on the list

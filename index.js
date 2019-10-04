fc298aac2d107b452d0d269691d80643c443890f
Travis Hanneman <thanneman1@gmail.com>
2019-10-04 16:24:15 -0700

    updated the index.html and index.js to store data in an object and update that object if needed.

--- index.js
+++ index.js
@@ -1,32 +1,103 @@
-function shoppingList () {
+'use strict';
 
-    // Add items to the list
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
+const STORE = [
+  {id: cuid(), name: "apples", checked: false},
+  {id: cuid(), name: "oranges", checked: false},
+  {id: cuid(), name: "milk", checked: true},
+  {id: cuid(), name: "bread", checked: false}
+];
 
-        const newItem = $(this).find('#shopping-list-entry').val();
 
-        $('.shopping-list').append(`<li>
-            <span class="shopping-item">${newItem}</span>
-            <div class="shopping-item-controls">
-                <button class="shopping-item-toggle">
-                    <span class="button-label">check</span>
-                </button>
-                <button class="shopping-item-delete">
-                    <span class="button-label">delete</span>
-                </button>
-            </div>
-        </li>`);
-    });
-
-    // Check an item on the list
-    $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
-        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
-    });
-
-    // Remove item from the list
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
-    });
+function generateItemElement(item) {
+  return `
+    <li data-item-id="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
 }
-$(shoppingList);
+
+
+function generateShoppingItemsString(shoppingList) {
+
+  const items = shoppingList.map((item) => generateItemElement(item));
+  
+  return items.join("");
+}
+
+
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+
+function addItemToShoppingList(itemName) {
+  STORE.push({id: cuid(), name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
+function toggleCheckedForListItem(itemId) {
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
+
+
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
+}
+
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    renderShoppingList();
+  });
+}
+
+function removeListItem(itemId) {
+  const itemIndex = STORE.findIndex(item => item.id === itemId);
+  STORE.splice(itemIndex, 1);
+}
+
+function handleDeleteItemClicked() {
+  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+    const itemIndex = getItemIdFromElement(event.currentTarget);
+    removeListItem(itemIndex);
+    renderShoppingList();   
+  });
+}
+
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
